e9bf23edf780e27f466e615356953ae179b67442
xyhuuz <tdcqxyh@outlook.com>
2025-12-25 12:45:46 +0800

    first_commit

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $(".open-btn").click(function() {
+        $(".modal").fadeIn();
+    });
+
+    $(".close-btn").click(function() {
+        $(".modal").fadeOut();
+    });
+});
